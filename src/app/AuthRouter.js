c459be31e629e33b8b97e78ded3d848c9cef430b
donaldtrieu <donaldtrieu@gmail.com>
2021-06-23 01:11:25 +0700

    commit

--- src/app/AuthRouter.js
+++ src/app/AuthRouter.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { Redirect, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from "react-redux";
+import PropTypes from 'prop-types';
+
+const renderMergedProps = (component, ...rest) => {
+    const finalProps = Object.assign({}, ...rest);
+    return React.createElement(component, finalProps);
+};
 
 const AuthRouter = ({ component: Component, ...rest }) => {
     const isLogin = useSelector((state) => state.login.checkLogin);
     return (
         isLogin ? (
-            <Route>
-                <Component path={rest.path} />
-            </Route>
+            <Route
+                {...rest}
+                render={(props) =>
+                    renderMergedProps(Component, props, rest)
+                }>
+            </Route >
         ) : (
             <Redirect to="/login" />
         )
     )
 }
+
+AuthRouter.propTypes = {
+    component: PropTypes.oneOfType([PropTypes.elementType, PropTypes.node])
+};
 
 export default AuthRouter
