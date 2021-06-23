7051d2fd66d8ec7b561766a414cd318de0c9e303
donaldtrieu <donaldtrieu@gmail.com>
2021-06-23 16:52:50 +0700

    commit

--- src/constants/baseURl.js
+++ src/constants/baseURl.js
@@ -1,6 +1,6 @@
 export const FILE_URL =
   process.env.REACT_APP_BASE_URL_CDN + "/files" ||
   "https://cdn.tingtong.xyz/files";
-export const BASE_URL = process.env.REACT_APP_BASE_URL + "/api" || "";
-export const SOCKET_URL =
+  export const BASE_URL = process.env.REACT_APP_BASE_URL + ":" + process.env.REACT_APP_PORT + "/api" || "";
+  export const SOCKET_URL =
   process.env.REACT_APP_BASE_URL_SOCKET || "socket.tingtong.xyz";
