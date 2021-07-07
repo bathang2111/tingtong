1d7b003c90285cbed3b37a41e63ecd02974228d2
Bathang2111 <bathangg20111998@gmail.com>
2021-07-08 01:41:00 +0700

    commit

--- src/constants/baseURl.js
+++ src/constants/baseURl.js
@@ -8,6 +8,6 @@ export const BASE_URL = process.env.REACT_APP_BASE_URL + "/api" || "";
 export const SOCKET_URL =
   process.env.REACT_APP_BASE_URL_SOCKET || "socket.tingtong.xyz";
 export const BASE_URL_WINDOW_CALL = process.env.REACT_APP_BASE_URL_WINDOW_CALL; //+ ":" + process.env.PORT;
-// export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
-export const CLIENT_ID =
-  "1066624920333-den5ior9urs5e52ea9tgpr5ml90pgc7h.apps.googleusercontent.com";
+export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+// export const CLIENT_ID =
+//   "1066624920333-den5ior9urs5e52ea9tgpr5ml90pgc7h.apps.googleusercontent.com";
